b7fb5fb9c3a0883a288288a7117be93fccbdb533
sulimonsta <145644480+sulimonsta@users.noreply.github.com>
2023-10-10 23:17:41 -0700

    Add files via upload

---
+++ Findandremove.js
@@ -0,0 +1,18 @@
+// findUserByUsername solution
+
+function findUserByUsername(usersArray, username) {
+    return usersArray.find(function(user){
+      return user.username === username;
+    })
+  }
+
+  // removeUser solution
+  
+  function removeUser(usersArray, username) {
+    let foundIndex = usersArray.findIndex(function(user){
+      return user.username === username;
+    })
+    if(foundIndex === -1) return;
+  
+    return usersArray.splice(foundIndex,1)[0];
+  }
