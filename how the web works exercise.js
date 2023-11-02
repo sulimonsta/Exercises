68d5e8f0a9af7c2a14b4b4895b041d378c6333b6
sulimonsta <145644480+sulimonsta@users.noreply.github.com>
2023-11-01 19:38:24 -0700

    Add files via upload

---
+++ how the web works exercise.js
@@ -0,0 +1,16 @@
+HTTP: controls how clients get data from or send data to a server
+URL: an address for a webpage/internet source
+DNS: takes URLS and converts them to IP addresses
+query string: allows you to pass key pairs into url format
+
+GET - gets data from server
+POST - sends data to server
+
+HTTP request: request from client to a server using HTTP protocol
+HTTP response: response from server to client using HTTP protocol
+
+HTTP header: they provide extra information about request or responses
+    request: host, cookie, accept
+    request: last-modified, set-cookie, cache-controls
+
+URL in browser: browser takes the name and turns it into an IP address, makes a request, and a response is given by the server. The browser takes the informatin given and receives responses for varius requests.
