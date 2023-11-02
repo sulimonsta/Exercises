897eba57168c09f50d207096e8b4b1181ffc81af
sulimonsta <145644480+sulimonsta@users.noreply.github.com>
2023-11-01 18:16:27 -0700

    Add files via upload

---
+++ let and const exercise.js
@@ -0,0 +1,7 @@
+you can reassign with var but not let.
+
+you can reassign with var but not redclare or reassign with const.
+
+you can reassign with let but not redeclare with const.
+
+hoisting explains how the javascript compiler functions. 
